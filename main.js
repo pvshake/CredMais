1db4fcb28564fe3d0d9d3dd74fec7bad28b92ab0
pvshake <pvshakeu@gmail.com>
2022-03-10 18:25:43 -0300

    commit final version white

--- main.js
+++ main.js
@@ -64,6 +64,44 @@ scrollReveal.reveal(
   { interval: 225 }
 )
 
+/* Menu ativo conforme a seção visível na página */
+
+
+
+
+
+
+
+
+const sections = document.querySelectorAll('main section[id]')
+
+function activatemenuAtCurrenteSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight/8)*4
+
+  for(const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if(checkpointStart && checkpointEnd){
+      document
+      .querySelector('nav ul li a[href*=' + sectionId + ']')
+      .classList.add('active')
+    }else{
+      document
+      .querySelector('nav ul li a[href*=' + sectionId + ']')
+      .classList.remove('active')
+    }
+  }
+}
+
+window.addEventListener('scroll', function () {
+  activatemenuAtCurrenteSection();
+})
+
 /*const cards = [
   {
     titulo: "Crédito Consignado",
